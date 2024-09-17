0a541204b9133a61030578caa0401d6805f9b24a
Nimish Narang <nimish.narang@pela.earth>
2024-09-17 14:53:57 -0700

    Create WeekOverviewViewModel

--- WeeklyPlanner/WeeklyPlanner/screens/main/weekOverview/WeekOverviewScreen.swift
+++ WeeklyPlanner/WeeklyPlanner/screens/main/weekOverview/WeekOverviewScreen.swift
@@ -12,91 +12,42 @@ import SwiftUI
 struct WeekOverviewScreen: View {
     @FetchRequest(sortDescriptors: []) var dailySchedules: FetchedResults<DailySchedule>
     
-    var goals: [Goal] {
-        dailySchedules.reduce(NSMutableOrderedSet()) {
-            if let dailyGoals = $1.goals {
-                $0.addObjects(from: dailyGoals.array)
-            }
-            return $0
-        }.array as? [Goal] ?? []
-    }
-    var toDoItems: [ToDoItem] {
-        dailySchedules.reduce(NSMutableOrderedSet()) {
-            if let dailyToDoItems = $1.toDoItems {
-                $0.addObjects(from: dailyToDoItems.array)
-            }
-            return $0
-        }.array as? [ToDoItem] ?? []
-    }
-    var toBuyItems: [ToBuyItem] {
-        dailySchedules.reduce(NSMutableOrderedSet()) {
-            if let dailyToBuyItems = $1.toBuyItems {
-                $0.addObjects(from: dailyToBuyItems.array)
-            }
-            return $0
-        }.array as? [ToBuyItem] ?? []
-    }
-    var meals: [Meal] {
-        dailySchedules.reduce(NSMutableOrderedSet()) {
-            if let dailyMeals = $1.meals {
-                $0.addObjects(from: dailyMeals.array)
-            }
-            return $0
-        }.array as? [Meal] ?? []
-    }
-    var workouts: [Workout] {
-        dailySchedules.reduce(NSMutableOrderedSet()) {
-            if let dailyWorkouts = $1.workouts {
-                $0.addObjects(from: dailyWorkouts.array)
-            }
-            return $0
-        }.array as? [Workout] ?? []
-    }
-    var notes: String {
-        return dailySchedules.reduce("") {
-            if let notes = $1.notes {
-                return $0 + notes + "\n"
-            } else {
-                return $0 + ""
-            }
-        }
-    }
+    @StateObject private var viewModel = WeekOverviewViewModel()
+    
         
     var body: some View {
         NavigationView {
             ScrollView(showsIndicators: false) {
-                VStack(spacing: 0) {
-                    VStack(spacing: 40) {
-                        // Goals
-                        WeekItemsListView(
-                            tasksType: .goal,
-                            taskItems: Array(goals)
-                        )
-                        // To do list
-                        WeekItemsListView(
-                            tasksType: .toDo,
-                            taskItems: Array(toDoItems)
-                        )
-                        // To buy list
-                        WeekItemsListView(
-                            tasksType: .toBuy,
-                            taskItems: Array(toBuyItems)
-                        )
-                        // Meals
-                        WeekItemsListView(
-                            tasksType: .meal,
-                            taskItems: Array(meals)
-                        )
-                        // Workouts
-                        WeekItemsListView(
-                            tasksType: .workout,
-                            taskItems: Array(workouts)
-                        )
-                        // Notes
-                        WeekOverviewNotesView(text: notes)
-                    }
-                    .padding(20)
+                VStack(spacing: 40) {
+                    // Goals
+                    WeekItemsListView(
+                        tasksType: .goal,
+                        taskItems: viewModel.goals
+                    )
+                    // To do list
+                    WeekItemsListView(
+                        tasksType: .toDo,
+                        taskItems: viewModel.toDoItems
+                    )
+                    // To buy list
+                    WeekItemsListView(
+                        tasksType: .toBuy,
+                        taskItems: viewModel.toBuyItems
+                    )
+                    // Meals
+                    WeekItemsListView(
+                        tasksType: .meal,
+                        taskItems: viewModel.meals
+                    )
+                    // Workouts
+                    WeekItemsListView(
+                        tasksType: .workout,
+                        taskItems: viewModel.workouts
+                    )
+                    // Notes
+                    WeekOverviewNotesView(text: viewModel.notes)
                 }
+                .padding(20)
             }
             .frame(
                 minWidth: 0,
@@ -112,6 +63,9 @@ struct WeekOverviewScreen: View {
             }
             .toolbarBackground(.visible, for: .navigationBar)
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                viewModel.setDailySchedules(dailySchedules: Array(dailySchedules))
+            }
         }
     }
 }
